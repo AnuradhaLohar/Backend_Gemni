c8ac17f6bacc93653b91555d2b5d85c2e6f523c5
AnuradhaLohar <loharanuradha644@gmail.com>
2025-12-18 16:14:23 +0530

    changes

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ import cors from 'cors'
 const app = express()
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  // origin: "http://localhost:5173",
+  origin: "https://frontend-gemini-eight.vercel.app",
   credentials : true
 }))
 
